3d75445251af155687329e5b29a40f93599b5e01
yizhixianyuuuu <dding4673467@163.com>
2024-11-30 17:43:07 +0800

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -23,8 +23,8 @@ export default defineConfig({
     sidebar: false, // 关闭侧边栏
     aside: "left", // 设置右侧侧边栏在左侧显示
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/sysyz-itcom' }
-    ]
+//    socialLinks: [
+//      { icon: 'github', link: 'https://github.com/sysyz-itcom' }
+//    ]
   }
 })
